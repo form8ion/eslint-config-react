90e44558f68d179021180274684ff87f24744419
Matt Travi <programmer@travi.org>
2019-05-30 22:41:55 -0500

    feat(storybook): defined storybook rules
    as a glob to simplify the config for consumers

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': ['error', {extensions: ['.js']}],
     'react/require-default-props': 'off'
-  }
+  },
+
+  overrides: [{
+    files: '.storybook/*.js',
+    rules: {
+      'import/no-extraneous-dependencies': ['error', {devDependencies: true}]
+    }
+  }]
 };
